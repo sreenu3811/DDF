1e634f6088a6655f5da16a029ab0bb27f93ad70a
sreenu <sreenu.surabhi04@gmail.com>
2018-08-21 17:32:01 +0530

    add firstime

---
+++ src/main/java/sreenu/com/DataDrivenFrameWork/App.java
@@ -0,0 +1,13 @@
+package sreenu.com.DataDrivenFrameWork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
